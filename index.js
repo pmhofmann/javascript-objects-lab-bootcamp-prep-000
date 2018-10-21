2a1575bd29ea0081346f8f9c92cc89addf8295e6
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-21 11:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {Pizza:["Dough","Tomatoes","Cheese","Basil"];
   }
 function updateObjectWithKeyAndValue(recipes, key, value){
-  return(Object.assign({}, recipes, ))
+  return(Object.assign({}, recipes, {[key]}))
 }
